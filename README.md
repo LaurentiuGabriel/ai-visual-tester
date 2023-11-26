53da94538caaf52a94d634990a7d1ee499b67e4b
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2023-11-26 22:13:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # ai-visual-tester
+
+ai-visual-tester is an innovative npm package that leverages artificial intelligence to perform visual testing on screenshots of applications. By utilizing OpenAI's powerful models, this package can analyze screenshots for visual inconsistencies, UX issues, typos, and other elements critical to high-quality software development.
+
+## Features
+- AI-Powered Analysis: Uses OpenAI's GPT models to analyze application screenshots.
+- Easy to Use: Simple interface for passing screenshots and receiving AI analysis.
+- Flexible: Can be integrated into various stages of the development and testing process.
+
+## Installation
+To use ai-visual-tester, first ensure you have Node.js installed. Then, run the following command in your project directory:
+
+```
+npm install ai-visual-tester
+```
+You also need an OpenAI API key stored as an environment variable with this key: OPENAI_KEY.
+
+## Usage
+Import the testPhoto function from the package and use it to send a file for analysis. The function expects a file as an argument.
+
+### Example
+```
+const { testPhoto } = require('ai-visual-tester');
+const fs = require('fs');
+
+// Read the image file as a buffer
+const fileBuffer = fs.readFileSync('path_to_your_screenshot.jpg');
+
+testPhoto(fileBuffer)
+  .then(lines => {
+    console.log("AI Analysis:");
+    lines.forEach(line => console.log(line));
+  })
+  .catch(error => console.error("Error:", error));
+```
+
+## API Reference
+### testPhoto(file)
+- file (File): The screenshot file to be analyzed.
+
+## Contributing
+Contributions are welcome! If you'd like to contribute, feel free to fork the repository and submit a pull request.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Acknowledgments
+This package utilizes OpenAI's API for AI-powered analysis.
+Thanks to all the contributors who have helped improve ai-visual-tester.
---
+++ index.js
@@ -0,0 +1,74 @@
+const fetch = require('node-fetch');
+
+/**
+ * Encodes the provided file into a base64 string.
+ * @param {File} file - The file to be encoded.
+ * @returns {Promise<string>} A promise that resolves to the base64 encoded string.
+ */
+function encodeImage(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            const base64String = reader.result.replace(/^data:.+;base64,/, '');
+            resolve(base64String);
+        };
+
+        reader.onerror = reject;
+
+        reader.readAsDataURL(file);
+    });
+}
+
+/**
+ * Sends a chat request to OpenAI using an image file.
+ * @param {File} file - The image file to be analyzed.
+ * @returns {Promise<string[]>} A promise that resolves to the chat response lines.
+ */
+async function testPhoto(file) {
+    const base64Image = await encodeImage(file);
+
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.OPENAI_KEY}`
+    };
+
+    const payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Act as a top quality engineer. I want you to analyze what is in this image. It is supposed to be the user interface of an application. Check for visual isues, incosistencies, UX issues. You don't need context, you just judge by what you see. Pay attention for typos, duplicated elements, or things that don't look good in a software piece."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": `data:image/jpeg;base64,${base64Image}`
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    };
+
+    try {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(payload)
+        });
+
+        const responseJson = await response.json();
+        const messageContent = responseJson.choices[0].message.content;
+        return messageContent.split('\n');
+    } catch (error) {
+        console.error("Error during API request:", error);
+        throw error;
+    }
+}
+
+module.exports = { testPhoto, encodeImage };
